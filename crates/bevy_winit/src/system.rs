c5588578731ba5297e166f50c888b2effb30f1ff
Kafva <kafva.one@gmail.com>
2025-05-21 06:52:02 +0200

    Silence log spam for iOS simulator

--- crates/bevy_winit/src/system.rs
+++ crates/bevy_winit/src/system.rs
@@ -225,7 +225,8 @@ pub fn create_monitors(
             idx += 1;
             true
         } else {
-            info!("Monitor removed {}", entity);
+            // TODO: iOS
+            // info!("Monitor removed {}", entity);
             commands.entity(*entity).despawn();
             idx += 1;
             false
